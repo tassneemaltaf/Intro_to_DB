1d600ec4d7a4715f0bf452ab5aee6eb5a19b8d71
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-02-03 12:17:32 +0100

    "First task added"

---
+++ alx_book_store.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store
+
+CREATE TABLE Books (
+  book_id INT PRIMARY KEY,
+  title VARCHAR(130),
+  author_id FOREIGN KEY REFERENCES Authors(author_id),
+  price DOUBLE,
+  publication_date DATE
+);
+
+CREATE TABLE Authors (
+  author_id PRIMARY KEY
+  author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+  customer_id PRIMARY KEY,
+  customer_name VARCHAR(215),
+  email VARCHAR(215),
+  address TEXT
+);
+
+CREATE TABLE Orders (
+  order_id PRIMARY KEY
+  customer_id FOREIGN KEY REFERENCES Customers(customer_id),
+  order_date DATE
+);
+
+CREATE TABLE Order_Details (
+  order_detail_id PRIMARY KEY,
+  order_id FOREIGN KEY REFERENCES Orders(order_id),
+  book_id FOREIGN KEY REFERENCES Books(book_id),
+  quantity DOUBLE
+)
