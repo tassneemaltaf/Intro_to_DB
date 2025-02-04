d6f0da422e271e12ea3e46903d9940eeee084100
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-02-04 18:47:12 +0100

    Simple edit

--- MySQLServer.py
+++ MySQLServer.py
@@ -9,7 +9,7 @@ my_db = mysql.connector.connect(
 
 my_cursor = my_db.cursor()
 try:
-  my_cursor.execute("CREATE DATABASE alx_book_store")
+  my_cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
   print("Database 'alx_book_store' created successfully!")
 except:
   print("Database already exists")
