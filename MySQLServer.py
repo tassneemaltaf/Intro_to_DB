edb7d8b3d0283b598ce17ad804e7db35577dca3b
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-02-04 18:46:01 +0100

    Second task added

---
+++ MySQLServer.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+my_db = mysql.connector.connect(
+    host="localhost",
+    user="tassneem",
+    password="Tassneem:1",
+    database="alx_book_store"
+  )
+
+my_cursor = my_db.cursor()
+try:
+  my_cursor.execute("CREATE DATABASE alx_book_store")
+  print("Database 'alx_book_store' created successfully!")
+except:
+  print("Database already exists")
+
+
+my_cursor.close()
+my_db.close()
