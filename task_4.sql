77a094cc2c997db52504d5abb7c6da68949ae5e7
Tassneem Altaf <tassneemomadealtaf@gmail.com>
2025-02-05 15:56:46 +0100

    Third attempt

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 
 SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = "alx_book_store" AND TABLE_NAME = "Books";
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
  
